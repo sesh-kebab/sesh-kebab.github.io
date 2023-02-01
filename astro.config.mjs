085519fa26473abab6f19a8c71f822c7d242502d
Seshi Chemudugunta <seshic@microsoft.com>
2023-02-01 11:42:51 -0800

    Remove initial boilerplate.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sesh-kebab.github.io',
-  base: '/sesh-kebab.github.io',
+  base: '/',
 	integrations: [mdx(), sitemap()],
 });
